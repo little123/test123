f2e15fbcd5e80c316232034e57fd409d2be59c86
little123 <544764543@qq.com>
2015-07-02 15:46:41 +0800

    创建项目

---
+++ src/PassWordDigester.java
@@ -0,0 +1,54 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+
+public class PassWordDigester {
+	
+	
+	 public static String getPassMD5(String pass) {
+	        String keys = null;
+	        try {
+	            MessageDigest md = MessageDigest.getInstance("MD5");
+	            if (pass == null) {
+	                pass = "";
+	            }
+	            byte[] bPass = pass.getBytes("UTF-8");
+	            md.update(bPass);
+//	            keys = new String(md.digest(), "GBK");
+	            keys = bytesToHexString(md.digest());
+	        }
+	        catch (NoSuchAlgorithmException aex) {
+	        	aex.printStackTrace();
+	        }
+	        catch (java.io.UnsupportedEncodingException uex) {
+	        	uex.printStackTrace();
+	        }
+	        return keys;
+	    }
+	     
+	    /**
+	     * 将beye[]转换为十六进制字符串
+	     * @param bArray
+	     * @return
+	     */
+	   public static final String bytesToHexString(byte[] bArray) {
+	     StringBuffer sb = new StringBuffer(bArray.length);
+	     String sTemp;
+	     for (int i = 0; i < bArray.length; i++) {
+	      sTemp = Integer.toHexString(0xFF & bArray[i]);
+	      if (sTemp.length() < 2){
+	       sb.append(0);
+	      }
+	      sb.append(sTemp.toUpperCase());
+	     }
+	     return sb.toString();
+	  }
+	 
+	 
+	  public static void main(String[] args){
+	   String a = "qazwsx";
+	   System.out.println(getPassMD5(a));
+	   
+	  }
+	}
+
