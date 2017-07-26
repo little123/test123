09e4d4aa88384a7f019b164d5f6d9ce5021ddab7
liu_rui <liu_rui@Ctrip.com>
2017-07-26 10:34:06 +0800

    冲突测试2

--- src/PassWordDigester.java
+++ src/PassWordDigester.java
@@ -3,49 +3,53 @@ import java.security.NoSuchAlgorithmException;
 
 public class PassWordDigester {
 
-	
-	 public static Integer getPassMD5(String password) {
-	        String keys = null;
-	        try {
-	            MessageDigest md = MessageDigest.getInstance("MD5");
-	            if (password == null) {
-					keys = "";
-	            }
-	            byte[] bPass = password.getBytes("UTF-8");
-	            md.update(bPass);
-	            keys = bytesToHexString(md.digest());
-	        }
-	        catch (NoSuchAlgorithmException aex) {
-	        	aex.printStackTrace();
-	        }
-	        catch (java.io.UnsupportedEncodingException uex) {
-	        	uex.printStackTrace();
-	        }
-	        return Integer.getInteger(keys);
-	    }
-	     
-	    /**
-	     * @param bArray
-	     * @return
-	     */
-	   public static final String bytesToHexString(byte[] bArray) {
-	     StringBuffer sb = new StringBuffer(bArray.length);
-	     String sTemp;
-	     for (int i = 0; i < bArray.length; i++) {
-	      sTemp = Integer.toHexString(0xFF & bArray[i]);
-	      if (sTemp.length() < 2){
-	       sb.append(0);
-	      }
-	      sb.append(sTemp.toUpperCase());
-	     }
-	     return sb.toString();
-	  }
-	 
-	 
-	  public static void main(String[] args){
-	   String a = "qazwsx";
-	   System.out.println(getPassMD5(a));
-	   System.out.println("再次测试!!!");
-	  }
-	}
+
+    public Integer getPassMD5(String password) {
+        Integer keys = Integer.valueOf(password);
+        String keyStr = "";
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            if (password == null) {
+                keys = 0;
+            }
+            byte[] bPass = password.getBytes("UTF-8");
+            md.update(bPass);
+            keyStr = bytesToHexString(md.digest());
+        } catch (NoSuchAlgorithmException aex) {
+            aex.printStackTrace();
+        } catch (java.io.UnsupportedEncodingException uex) {
+            uex.printStackTrace();
+        }
+        return new Integer(keyStr);
+    }
+
+    /**
+     * @param bArray
+     * @return
+     */
+    public static final String bytesToHexString(byte[] bArray) {
+        StringBuffer sb = new StringBuffer(bArray.length);
+        String sTemp;
+        for (int i = 0; i < bArray.length; i++) {
+            sTemp = Integer.toHexString(0xFF & bArray[i]);
+            if (sTemp.length() < 2) {
+                sb.append(0);
+            }
+            sb.append(sTemp.toUpperCase());
+        }
+        return sb.toString();
+    }
+
+
+    public static void main(String[] args) {
+        String a = "qazwsx";
+        PassWordDigester passWordDigester = new PassWordDigester();
+        String keys = passWordDigester.getPassMD5(a).toString();
+        System.out.println(keys);
+        StringBuffer buffer = new StringBuffer();
+        buffer.append(a);
+        buffer.append(keys);
+        System.out.println(buffer.toString());
+    }
+}
 
